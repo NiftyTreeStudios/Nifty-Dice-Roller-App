9c4b6f971fa398cca6426dddddfc475df4886532
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-06-11 11:14:38 +0200

    Initial Commit

---
+++ Nifty-Dice-Roller-App/Nifty_Dice_Roller_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nifty_Dice_Roller_AppApp.swift
+//  Nifty-Dice-Roller-App
+//
+//  Created by Iiro Alhonen on 11.06.21.
+//
+
+import SwiftUI
+
+@main
+struct Nifty_Dice_Roller_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
